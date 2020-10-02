1c196b9546faf3d6e5b7b4d5ee4b647222cb2200
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-02 13:19:44 +0200

    dev

--- resources/js/components/Form.vue
+++ resources/js/components/Form.vue
@@ -1,30 +1,30 @@
 <template>
-    <form class="form-template" @submit.prevent="submitForm">
-        <div class="flex flex-row">
-            <vue-country-code @onSelect="onCountrySelect" />
-            <masked-input
-                type="tel"
-                autocomplete="off"
-                placeholder="Ваш телефон*"
-                class="w-full ml-4 pl-4"
-                v-model="$v.phone.$model"
-                :mask="{
-                    pattern: '(V11) 111-11-11',
-                    formatCharacters: {
-                        V: {
-                            validate: char => /[0-9]/.test(char)
-                        }
-                    }
-                }"
-                @focus.native="(isValid = true), (onFocus = true)"
-                @blur.native="onFocus = false"
-            />
-        </div>
+  <form class="form-template" @submit.prevent="submitForm">
+    <div class="flex flex-row">
+      <vue-country-code @onSelect="onCountrySelect" />
+      <masked-input
+        type="tel"
+        autocomplete="off"
+        placeholder="Your phone*"
+        class="w-full ml-4 pl-4"
+        v-model="$v.phone.$model"
+        :mask="{
+          pattern: '(V11) 111-11-11',
+          formatCharacters: {
+            V: {
+              validate: (char) => /[0-9]/.test(char),
+            },
+          },
+        }"
+        @focus.native="(isValid = true), (onFocus = true)"
+        @blur.native="onFocus = false"
+      />
+    </div>
 
-        <button class="button-pulse">
-            <span>Получить консультацию и рассчёт</span>
-        </button>
-    </form>
+    <button class="button-pulse">
+      <span>Get Consultation and Price</span>
+    </button>
+  </form>
 </template>
 
 <script>
@@ -36,62 +36,62 @@ import { mapActions, mapGetters } from "vuex";
 //валидация телефона по регулярному вырожению
 ///^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
 const phoneValidat = helpers.regex(
-    "phoneValidat",
-    /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
+  "phoneValidat",
+  /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
 );
 
 export default {
-    data: () => ({
-        phone: "",
-        url: "lmr.vskidke.ru",
-        dialCode: "",
-        isValid: true,
-        onFocus: false
-    }),
-    components: { MaskedInput },
-    mounted() {
-        //console.log("Component mounted.");
+  data: () => ({
+    phone: "",
+    url: "lmr.vskidke.ru",
+    dialCode: "",
+    isValid: true,
+    onFocus: false,
+  }),
+  components: { MaskedInput },
+  mounted() {
+    //console.log("Component mounted.");
+  },
+  computed: {
+    ...mapGetters(["isModal", "isSuccess"]),
+    phoneNumber: function () {
+      return "+" + this.dialCode + " " + this.phone;
     },
-    computed: {
-        ...mapGetters(["isModal", "isSuccess"]),
-        phoneNumber: function() {
-            return "+" + this.dialCode + " " + this.phone;
-        }
-    },
-    methods: {
-        ...mapActions(["setModal", "unsetModal", "setSuccess", "unsetSuccess"]),
-        submitForm() {
-            if (this.$v.phone.$invalid) {
-                this.isValid = false;
-            } else {
-                this.isValid = true;
-                //fbq("track", "Lead");
-                axios
-                    .post("api/lead", {
-                        phone: this.phoneNumber,
-                        url: this.url
-                    })
-                    .then(response => {
-                        console.log(response);
+  },
+  methods: {
+    ...mapActions(["setModal", "unsetModal", "setSuccess", "unsetSuccess"]),
+    submitForm() {
+      if (this.$v.phone.$invalid) {
+        this.isValid = false;
+      } else {
+        this.isValid = true;
+        //fbq("track", "Lead");
+        axios
+          .post("api/lead", {
+            phone: this.phoneNumber,
+            url: this.url,
+          })
+          .then((response) => {
+            console.log(response);
 
-                        this.phone = "";
-                        this.setSuccess();
-                        this.setModal();
-                    });
-                // this.phone = "";
-                // this.setSuccess();
-                // this.setModal();
-            }
-        },
-        onCountrySelect({ name, iso2, dialCode }) {
-            this.dialCode = dialCode;
-        }
+            this.phone = "";
+            this.setSuccess();
+            this.setModal();
+          });
+        // this.phone = "";
+        // this.setSuccess();
+        // this.setModal();
+      }
     },
-    validations: {
-        phone: {
-            required,
-            phoneValidat
-        }
-    }
+    onCountrySelect({ name, iso2, dialCode }) {
+      this.dialCode = dialCode;
+    },
+  },
+  validations: {
+    phone: {
+      required,
+      phoneValidat,
+    },
+  },
 };
 </script>
