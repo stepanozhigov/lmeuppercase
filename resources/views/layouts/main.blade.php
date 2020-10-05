a8350f6220e048ceed64320ee4bd135be3843dae
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-05 11:41:10 +0200

    prod

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -57,11 +57,11 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '633447790647620');
+    fbq('init', '1741418499343983');
     fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=633447790647620&ev=PageView&noscript=1" /></noscript>
+            src="https://www.facebook.com/tr?id=1741418499343983&ev=PageView&noscript=1" ; /></noscript>
     <!-- End Facebook Pixel Code -->
 
 </body>
